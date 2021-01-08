01ce4e7f4d84915d342863cf10e2765866b02a0f
OksanaBakuta <oksanabakuta@gmail.com>
2021-01-08 20:48:38 +0200

    added section team

--- app/js/main.js
+++ app/js/main.js
@@ -2,8 +2,18 @@ $(function(){
   
   $('.slider').slick({
     dots: true,
+    arrows: false,
     fade: true,
     autoplay:true,
+    autoplaySpeed: 3000
+  });
+
+  $('.team__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    autoplay: true,
     autoplaySpeed: 3000
   });
 
