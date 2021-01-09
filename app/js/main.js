e22a7eb6ec4959895e9b2f7f74b5ef86b534a326
OksanaBakuta <oksanabakuta@gmail.com>
2021-01-09 17:55:35 +0200

    added section services

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 $(function(){
+
+  $('.tabs__btn').on('click', function(e){
+    e.preventDefault();
+    $('.tabs__btn').removeClass('tabs__btn--active');
+    $(this).addClass('tabs__btn--active');
+
+    $('.services__slider').removeClass('services__slider--active');
+    $($(this).attr('href')).addClass('services__slider--active');
+
+    $('.services__slider').slick('setPosition');
+  });
   
   $('.slider').slick({
     dots: true,
@@ -16,5 +27,14 @@ $(function(){
     autoplay: true,
     autoplaySpeed: 3000
   });
+  
+  $('.services__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false,
+    dots: true,
+  });
+
+
 
 });
